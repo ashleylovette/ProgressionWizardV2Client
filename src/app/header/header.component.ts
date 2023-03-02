70ea5d4076bcc2bf8acbaa6822ff8ca4ac31eed7
ashleylovette <ashleylovette15@gmail.com>
2023-03-01 18:39:10 -0600

    remove header html file, refactor saved chords dialog

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  template: `<mat-toolbar>
+                <span>Progression Wizard</span>
+                <span class="example-spacer"></span>
+                <mat-icon>menu</mat-icon>
+              </mat-toolbar>`,
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
