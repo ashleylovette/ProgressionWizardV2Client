222b19dbce9b9271a6742c3a2eb6424e34df45dc
ashleylovette <ashleylovette15@gmail.com>
2023-03-21 12:55:25 -0500

    add styling for landing page

--- src/app/landing-page/sign-on/sign-on.component.ts
+++ src/app/landing-page/sign-on/sign-on.component.ts
@@ -2,7 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-on',
-  template: ``,
+  template: `<mat-card class="sign-in-card">
+    <input matInput class="form-control" type="text" placeholder="Username or email">
+    <input matInput class="form-control" type="password" placeholder="Password">
+
+    <div class="existing-account">
+      <button type="submit" class="btn primary" matButton>Sign In</button>
+      <div>Forgot password?</div>
+    </div>
+    <hr>
+    <button matButton class="btn secondary create-account" type="button">Create Account</button>
+  </mat-card>`,
   styleUrls: ['./sign-on.component.scss']
 })
 export class SignOnComponent implements OnInit {
